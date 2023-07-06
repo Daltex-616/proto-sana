dde55cd631964e9d0412602c1ff77abeb9e1ca5b
Federico Nicolas Zamora <federicozamora616@gmail.com>
2023-07-06 12:27:36 -0300

    se eliminaron linas en la parte de noticias que no van

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -4,17 +4,6 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
 export function ListofNoticias({noticias}){
     return(
         <Container>
